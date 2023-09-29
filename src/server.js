368ab243574da1fa918ba9ddf680959987233179
Augusto Marsola <augusto.marsola@newfold.com>
2023-09-29 19:20:55 -0300

    added stream concepts

--- src/server.js
+++ src/server.js
@@ -1,8 +1,39 @@
 import http from 'node:http'
 
-const server = http.createServer((req, res) => {
-  return res.end('Hello World!');
-});
+const users = []
+
+const server = http.createServer(async (req, res) => {
+  const { url, method } = req
+
+  const buffer = []
+
+  for await (const chunk of req) {
+    buffer.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffer).toString())
+  } catch {
+    req.body = {}
+  }
+  
+  if (url === '/users' && method === 'GET') {
+    return res.end(JSON.stringify(users))
+  }
+
+  if (url === '/users' && method === 'POST') {
+    const { name, email } = req.body
+    
+    users.push({
+      name,
+      email
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
+})
 
 server.listen(3333, () => {
   console.log('Server is running on port 3333');
